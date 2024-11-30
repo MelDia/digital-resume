9323e2ffc7ee8e2339893b09ac3c6299677bf3de
meldia <diaz.melinajimena@gmail.com>
2024-11-30 18:27:34 -0300

    [DIGITAL RESUME] - Adjustment in changing dimensions on components

--- src/app/core/services/resize-service.service.ts
+++ src/app/core/services/resize-service.service.ts
@@ -16,13 +16,18 @@ export class DragResizeService {
   public resizeState$ = this.resizeStateSubject.asObservable();
 
   private readonly MIN_DIMENSIONS_PX = 50;
+  public MIN_WIDTH_PX: number = 200;
+  public MIN_HEIGHT_PX: number = 200;
 
   public validate(event: ResizeEvent): boolean {
+    const MIN_HEIGHT_PX = 300;
+    const MIN_WIDTH_PX = 300;
+
     if (
       event.rectangle.width &&
       event.rectangle.height &&
-      (event.rectangle.width < this.MIN_DIMENSIONS_PX ||
-        event.rectangle.height < this.MIN_DIMENSIONS_PX)
+      (event.rectangle.width < MIN_WIDTH_PX ||
+        event.rectangle.height < MIN_HEIGHT_PX)
     ) {
       return false;
     }
@@ -39,4 +44,30 @@ export class DragResizeService {
       overflow: 'hidden',
     };
   }
+
+  // public validate(event: ResizeEvent): boolean {
+  //   if (!event.rectangle) {
+  //     console.error('Resize event has no rectangle property');
+  //     return false;
+  //   }
+
+  //   if (!event.rectangle.width || !event.rectangle.height) {
+  //     console.error('Resize event has no valid width or height');
+  //     return false;
+  //   }
+
+  // const minWidth = this.MIN_WIDTH_PX;
+  // const minHeight = this.MIN_HEIGHT_PX;
+
+  //   if (
+  //     event.rectangle.width < minWidth ||
+  //     event.rectangle.height < minHeight
+  //   ) {
+  //     console.log('Validation Result: false');
+  //     return false;
+  //   }
+
+  //   console.log('Validation Result: true');
+  //   return true;
+  // }
 }
